095ed39e50511f4816e2eb8f17ee2c61218a1a6b
abhinavk123 <abhinavvv123@gmail.com>
2018-03-06 17:38:49 +0530

    UI for displaying result added

--- PredIt.py
+++ PredIt.py
@@ -34,6 +34,7 @@ class PredIt(QDialog,ui_predit.Ui_PredIt):
         self.algo_type.activated.connect(self.setalgotype)
         self.viewpushButton.clicked.connect(self.dispdataset)
         self.openpushButton.clicked.connect(self.open_file)
+        self.resultbutton.clicked.connect(self.display_result)
 
     def setalgotype(self):
         clicked = self.algo_type.currentIndex()
@@ -46,7 +47,7 @@ class PredIt(QDialog,ui_predit.Ui_PredIt):
             self.algo.addItems(self.clust_list)
 
     def dispdataset(self):
-        wid = QDialog()
+        wid = QDialog(self.sender())
         table = QTableWidget()
         table.setWindowTitle('hERO')
         table.setRowCount(self.table_row)
@@ -55,11 +56,15 @@ class PredIt(QDialog,ui_predit.Ui_PredIt):
             for j in range(self.table_col):
                 itemm = QTableWidgetItem(str(self.finaldataset[i][j]),)
                 table.setItem(i,j,itemm)
+        table.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+        table.sizeAdjustPolicy()
+        size = table.size()
         grid = QGridLayout()
-        grid.addWidget(table,0,0)
+        grid.addWidget(table)
         wid.setLayout(grid)
-        wid.setWindowTitle('Hello')
-        wid.setMinimumSize(table.horizontalHeader().length(),table.verticalHeader().length())
+        wid.setWindowTitle(os.path.basename(self.filename[0]))
+        wid.resize(size)
+        wid.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
         wid.exec_()
 
     @pyqtSlot(int)
@@ -78,6 +83,16 @@ class PredIt(QDialog,ui_predit.Ui_PredIt):
             self.finaldataset = self.dataset.iloc[:, :].values
             self.table_row = self.dataset.shape[0]
             self.table_col = self.dataset.shape[1]
+
+    def display_result(self):
+        dail = QDialog(self.sender())
+        tbrowser = QTextBrowser()
+        dail.setWindowTitle('Predit - Result')
+        layout = QGridLayout()
+        layout.addWidget(tbrowser)
+        dail.setLayout(layout)
+        tbrowser.setHtml('<html> <head> <!-- your webpage info goes here --> <title>My First Website</title> <meta name="author" content="your name" /> <meta name="description" content="" /> <!-- you should always add your stylesheet (css) in the head tag so that it starts loading before the page html is being displayed --> <link rel="stylesheet" href="style.css" type="text/css" /> </head> <body> <!-- webpage content goes here in the body --> <div id="page"> <div id="logo"> <h1><a href="/" id="logoLink">My First Website</a></h1> </div> <div id="nav"> <ul> <li><a href="#/home.html">Home</a></li> <li><a href="#/about.html">About</a></li> <li><a href="#/contact.html">Contact</a></li> </ul> </div> <div id="content"> <h2>Home</h2> <p> This is my first webpage! I was able to code all the HTML and CSS in order to make it. Watch out world of web design here I come! </p> <p> I can use my skills here to create websites for my business, my friends and family, my C.V, blog or articles. As well as any games or more experiment stuff (which is what the web is really all about). </p> </div> <div id="footer"> <p> Webpage made by <a href="/" target="_blank">[your name]</a> </p> </div> </div> </body> </html>')
+        dail.exec_()
 
 
 if __name__ == '__main__':
